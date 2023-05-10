3f3b2665cd856fbeac7f9d82a26d5035df55d8e5
tdreams <fabriels@gmail.com>
2023-05-10 08:59:30 -0700

    delete items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+export default function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuidv4(),
+      item,
+    };
+
+    if (item) {
+      setList([...list, newItem]);
+      setItem("");
+    }
+  };
+
+  console.log(list);
+
+  const deleteItem = (id) => {
+    setList(list.filter((el) => el.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button>Add Item</button>
+      </form>
+      <div>
+        {list.map((el) => {
+          return (
+            <div key={el.id}>
+              <p>{el.item}</p>
+              <button onClick={() => deleteItem(el.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
-
-export default App;
